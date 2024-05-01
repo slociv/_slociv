d2408a6e2aeed8e9ce597e07a1f5e4675edfc069
slociv <166732021+slociv@users.noreply.github.com>
2024-05-02 00:03:50 +0800

    c-3.2-5.1

---
+++ c/基础语法/3.2布尔和整型.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+int main() {
+	unsigned short salary, jiangjin{};
+	std::cout << "输入你的工资";
+	std::cin >> salary;
+	std::cout << "你的工资是" << salary << "属于找不到女朋友的水平";
+	std::cin >> jiangjin;
+	std::cout << "你的奖金是:" << jiangjin;
+}
+
+
+
+
+signed char 1字节  -128到127
+char                2的8次方个  可能是0到255
+
+short       2字节   -32768-32767 2  16次方
+short int 
+signed short
+singed short int
+
+int
+singed
+signed int
+
+
+
+long
+long int
+signed long
+signed long int  4字节
+
+long long
+
+long long int
+signed long long
+signed long long int   8 字节
+
+很重要 越省越好
+
+unsigned char    1字节  0到255
+
+unsigned short   2字节  0到65535
+wchar_t
+
